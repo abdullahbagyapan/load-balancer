c5543e2aeae9fc2fed7de4b98400dc2d2e3ef42c
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-01-29 21:16:40 +0300

    Add config variables
    -Define config variables
    --Listen Address
    --Servers addresses

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+var (
+	listenAddr = "localhost:8080"
+
+	servers = []string{
+		"localhost:5000",
+		"localhost:5001",
+		"localhost:5002",
+		"localhost:5003	",
+	}
+)
+
+func main() {
+
+}
